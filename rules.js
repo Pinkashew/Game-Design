808b7ac68d4ab398527747a47934f298ec13ab9e
Pinkashew <87673421+Pinkashew@users.noreply.github.com>
2024-04-17 21:55:35 -0700

    Add files via upload

--- rules.js
+++ rules.js
@@ -9,17 +9,43 @@ class Start extends Scene {
     }
 }
 
+var hasKey = false;
+
 class Location extends Scene {
     create(key) {
         let locationData = this.engine.storyData.Locations[key]; // TODO: use `key` to get the data object for the current story location
         this.engine.show(locationData.Body); // TODO: replace this text by the Body of the location data
-        
+
+        if("Warning" in locationData) {
+            this.engine.addChoice("Warning Sign", this.engine.storyData.Locations["Golem Temple Location"])
+            this.engine.show(locationData.Warning)
+        } 
+        else {
+            console.log("No warning")
+        }
+
+        if("Has key" in locationData) {
+            hasKey = true;
+            if (hasKey) {
+                console.log("Has key")
+            }
+        }
+        else {
+            if (hasKey) {
+                console.log("Has key")
+            }
+            else {
+                console.log("No key")
+            }
+        }
+
         if(locationData.Choices.length > 0) { // TODO: check if the location has any Choices
             for(let choice of locationData.Choices) { // TODO: loop over the location's Choices
                 this.engine.addChoice(choice.Text, choice); // TODO: use the Text of the choice
                 // TODO: add a useful second argument to addChoice so that the current code of handleChoice below works
             }
-        } else {
+        }
+        else {
             this.engine.addChoice("The end.")
         }
     }
